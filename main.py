4d7510c48c4a52f17cbee23c604154f82046fb31
KoliadaKol <koliadayaroslav2003@gmail.com>
2020-10-26 18:29:37 +1000

    26.10

--- main.py
+++ main.py
@@ -28,6 +28,15 @@ part2 = pygame.Surface((WIDTH_WIN // 2, HEIGHT_WIN))
 part2_rect = part2.get_rect(topleft=(WIDTH_WIN // 2, 0))
 part2.fill(BG_COLOR1)
 
+part3 = pygame.Surface((WIDTH_WIN // 2, HEIGHT_WIN // 5))
+part3_rect = part3.get_rect(topleft=(WIDTH_WIN // 2, 0))
+part3.fill(BG_COLOR2)
+
+k = pygame.image.load('start_button.png')
+k = pygame.transform.scale(k, (100, 100))
+k_rect = k.get_rect(center=(100, 100))
+part3.blit(k, k_rect)
+
 
 def line1():
     pygame.draw.rect(part1, pygame.Color('black'), [0, 0, WIDTH_WIN // 6 - 1.5, HEIGHT_WIN - 1.5], 3)
@@ -37,6 +46,13 @@ def line2():
     pygame.draw.rect(part2, pygame.Color('black'), [0, 0, WIDTH_WIN // 2, HEIGHT_WIN], 3)
 
 
+def line3():
+    pygame.draw.rect(screen, pygame.Color('black'), [WIDTH_WIN // 6, 0, WIDTH_WIN // 3, HEIGHT_WIN], 3)
+
+
+def line4():
+    pygame.draw.rect(part3, pygame.Color('black'),[0, 0, WIDTH_WIN, HEIGHT_WIN], 3)
+
 run = True
 while run: 
     for e in pygame.event.get():
@@ -45,6 +61,10 @@ while run:
     screen.fill(BG_COLOR)
     screen.blit(part1, part1_rect)
     screen.blit(part2, part2_rect)
+    screen.blit(part3, part3_rect)
     line1()
     line2()
+    line3()
+    line4()
+
     pygame.display.update()
