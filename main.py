58880f4429096e8c9f923ed03ce90480ea87ca98
KoliadaKol <koliadayaroslav2003@gmail.com>
2020-11-09 18:39:00 +1000

    1111

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ screen = pygame.display.set_mode((WIDTH_WIN, HEIGHT_WIN))
 pygame.display.set_icon(pygame.image.load('робот.png')) 
 pygame.display.set_caption('Стёпа-робот')
 
-i = 0
+i = [False, True]
+q = [False, True]
+w = [False, True]
 B = 0
 FPS = 60
 clock = pygame.time.Clock()
@@ -32,6 +34,7 @@ part2 = pygame.Surface((WIDTH_WIN // 2, HEIGHT_WIN))
 part2_rect = part2.get_rect(topleft=(WIDTH_WIN // 2, 0))
 part2.fill(BG_COLOR1)
 
+
 part3 = pygame.Surface((WIDTH_WIN // 2, HEIGHT_WIN // 6))
 part3_rect = part3.get_rect(topleft=(WIDTH_WIN // 2, 0))
 part3.fill(BG_COLOR2)
@@ -44,19 +47,6 @@ image_btn_rect = image_btn.get_rect(center = (100, 100))
 st_btn = pygame.image.load('pause_button.png')
 st_btn = pygame.transform.scale(st_btn, (90, 90))
 st_btn_rect = st_btn.get_rect(center = (100, 100))
-
-
-#pause = pygame.image.load('pause_button.png')
-#pause = pygame.transform.scale(pause, (100, 100))
-#pause_rect = pause.get_rect(center=(100, 100))
-
-#k = pygame.image.load('start_button.png')
-#k = pygame.transform.scale(k, (100, 100))
-#k_rect = k.get_rect(center=(100, 100))
-#part3.blit(k, k_rect)
-#mouse_pos = pygame.mouse.get_pos
-#if mouse_pos == k_rect:
-    ##part3.blit(pause, pause_rect)
 
 speed = pygame.image.load('speed_button.png')
 speed = pygame.transform.scale(speed, (100, 100))
@@ -73,6 +63,10 @@ res_rect = res.get_rect(center=(200, 90))
 res1= pygame.image.load('inv_restart_button.png')
 res1 = pygame.transform.scale(res1, (100, 100))
 res1_rect = res1.get_rect(center=(205, 90))
+
+BG_map= pygame.image.load('коридор.jpg')
+BG_map = pygame.transform.scale(BG_map, (part2.get_width(), part2.get_height() - part3.get_height()))
+BG_map_rect = BG_map.get_rect(topleft=(0, part3.get_height()))
 
 
 def line1():
@@ -98,32 +92,27 @@ while run:
         elif e.type == pygame.MOUSEBUTTONDOWN:
             if e.button == 1:
                 if btn1.collidepoint(e.pos):
-                    i = 1
+                    i.reverse()
                 if btn2.collidepoint(e.pos):
-                    i = 2
+                    w.reverse()
                 if btn3.collidepoint(e.pos):
-                    i = 3
+                    q.reverse()
+
     screen.fill(BG_COLOR)
     screen.blit(part1, part1_rect)
     screen.blit(part2, part2_rect)
     screen.blit(part3, part3_rect)
+    part2.blit(BG_map, BG_map_rect)
     line1()
     line2()
     line3()
     line4()
-    if i == 0 or i == 2 or i == 3:
-        btn1 = screen.blit(image_btn, (990,40))
-    if i == 0 or i == 2 or i == 1:
-        btn3 = screen.blit(res, (1100,40))
-    if i == 0 or i == 1 or i == 3:
-        btn2 = screen.blit(speed, (1800,40))
-    if i == 1:
-        screen.blit(st_btn, (990,40))
-    if i == 2:
-        screen.blit(speed1, (1805,40))
-    if i == 3:
-        screen.blit(res1, (1105,40))
+    btn1 = screen.blit(st_btn if i[0] else image_btn, (990,40))
+    btn3 = screen.blit(res1 if q[0] else res, (1100,40))
+    btn2 = screen.blit(speed1 if w[0] else speed, (1800,40))
     pygame.display.update()
-    if i == 1 or i == 2 or i == 3:
-        pygame.time.delay(1000)
-        i = 0
+    if q[0] == True:
+        pygame.time.delay(250)
+        q.reverse()
+    
+        
