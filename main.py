728c76cfebe1b6ef390ec36c8caa9644de2263f0
KoliadaKol <koliadayaroslav2003@gmail.com>
2020-11-11 18:32:47 +1000

    11.11

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import random
 import sys
 import os
 import time
-
+from Scripts import scripts
 
 pygame.init()
 os.environ['SDL_VIDEO_WINDOW_POS'] = '0,30'
 info = pygame.display.Info()
 WIDTH_WIN, HEIGHT_WIN = info.current_w, info.current_h
-
 
 screen = pygame.display.set_mode((WIDTH_WIN, HEIGHT_WIN))
 pygame.display.set_icon(pygame.image.load('робот.png')) 
@@ -39,35 +38,11 @@ part3 = pygame.Surface((WIDTH_WIN // 2, HEIGHT_WIN // 6))
 part3_rect = part3.get_rect(topleft=(WIDTH_WIN // 2, 0))
 part3.fill(BG_COLOR2)
 
-
-image_btn = pygame.image.load('start_button.png')
-image_btn = pygame.transform.scale(image_btn, (100, 100))
-image_btn_rect = image_btn.get_rect(center = (100, 100))
-
-st_btn = pygame.image.load('pause_button.png')
-st_btn = pygame.transform.scale(st_btn, (90, 90))
-st_btn_rect = st_btn.get_rect(center = (100, 100))
-
-speed = pygame.image.load('speed_button.png')
-speed = pygame.transform.scale(speed, (100, 100))
-speed_rect = speed.get_rect(center=(500, 90))
-
-speed1 = pygame.image.load('inv_speed_button.png')
-speed1 = pygame.transform.scale(speed1, (100, 100))
-speed1_rect = speed1.get_rect(center=(505, 90))
-
-res= pygame.image.load('restart_button.png')
-res = pygame.transform.scale(res, (100, 100))
-res_rect = res.get_rect(center=(200, 90))
-
-res1= pygame.image.load('inv_restart_button.png')
-res1 = pygame.transform.scale(res1, (100, 100))
-res1_rect = res1.get_rect(center=(205, 90))
+scripts()
 
 BG_map= pygame.image.load('коридор.jpg')
 BG_map = pygame.transform.scale(BG_map, (part2.get_width(), part2.get_height() - part3.get_height()))
 BG_map_rect = BG_map.get_rect(topleft=(0, part3.get_height()))
-
 
 def line1():
     pygame.draw.rect(part1, pygame.Color('black'), [0, 0, WIDTH_WIN // 6 - 1, HEIGHT_WIN - 1], 3)
@@ -83,6 +58,7 @@ def line3():
 
 def line4():
     pygame.draw.rect(part3, pygame.Color('black'),[0, 0, WIDTH_WIN, HEIGHT_WIN], 3)
+
 
 run = True
 while run: 
